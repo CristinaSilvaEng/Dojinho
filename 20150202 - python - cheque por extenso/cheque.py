4d32e0f8a3e07ff904a337f432b8a690484af667
CristinaSilvaEng <cristina.silva@openmailbox.org>
2015-02-05 21:15:23 -0200

    dois centavos

--- 20150202 - python - cheque por extenso/cheque.py
+++ 20150202 - python - cheque por extenso/cheque.py
@@ -1,4 +1,11 @@
 
 def cheque_por_extenso(valor):
 
-    return 'um centavo'
+    numero_por_extenso = ''
+
+    if valor == 0.01:
+        numero_por_extenso = 'um centavo'
+    elif valor == 0.02:
+        numero_por_extenso = 'dois centavos'
+
+    return numero_por_extenso
--- 20150202 - python - cheque por extenso/test_cheque.py
+++ 20150202 - python - cheque por extenso/test_cheque.py
@@ -6,4 +6,7 @@ class TestCheque(unittest.TestCase):
     def test_um_centavo(self):
         self.assertEquals(cheque_por_extenso(0.01), 'um centavo')
 
+    def test_dois_centavos(self):
+        self.assertEquals(cheque_por_extenso(0.02), 'dois centavos')
+
 unittest.main()
