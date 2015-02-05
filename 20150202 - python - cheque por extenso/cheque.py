da77913dbef6c2bc21b30b0e200530a4499bb02e
CristinaSilvaEng <cristina.silva@openmailbox.org>
2015-02-05 21:23:28 -0200

    refactoring

--- 20150202 - python - cheque por extenso/cheque.py
+++ 20150202 - python - cheque por extenso/cheque.py
@@ -1,11 +1,14 @@
 
 def cheque_por_extenso(valor):
 
+    unidades = ['centavo','centavos']
+    valores = ['um', 'dois']
+
     numero_por_extenso = ''
 
     if valor == 0.01:
-        numero_por_extenso = 'um centavo'
+        numero_por_extenso = valores[0] +' '+ unidades[0]
     elif valor == 0.02:
-        numero_por_extenso = 'dois centavos'
+        numero_por_extenso = valores[1] +' '+ unidades[1]
 
     return numero_por_extenso
